502a31a837ae3761256fa8fa3f456285c1333a9e
赵传龙 <zhaochuanlong@huan.tv>
2017-02-28 16:22:40 +0800

    Initial Commit

---
+++ SwiftMacro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftMacro
+//
+//  Created by 趙傳龍 on 2017/2/28.
+//  Copyright © 2017年 Huan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
