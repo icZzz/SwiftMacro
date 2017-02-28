499a2ad0505991ea6af4a98f58c59c37dc32adcb
赵传龙 <zhaochuanlong@huan.tv>
2017-02-28 17:16:33 +0800

    first commit

---
+++ SwiftMacro/UtilsMacro.swift
@@ -0,0 +1,86 @@
+//
+//  UtilsMacro.swift
+//
+//  Created by 趙傳龍 on 2017/2/28.
+//  Copyright © 2017年 Huan. All rights reserved.
+//
+
+import UIKit
+
+///屏幕宽高
+let VIEW_Height = UIScreen.main.bounds.size.height
+let VIEW_Width  = UIScreen.main.bounds.size.width
+
+///系统版本
+let SYSTEM_VERSION = UIDevice.current.systemVersion
+
+///判断是iPhone还是iPad
+let DEVICE_TYPE_PAD = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.pad ? true : false
+
+
+/// 显示log 信息
+func hLog(message: String, function: String = #function, file: String = #file, line: Int = #line){
+     print("Message \"\(message)\" (File: \(file), Function: \(function), Line: \(line))")
+}
+
+
+public extension UIColor {
+    
+    /// RGB颜色
+    public static func RGB(r red : CGFloat, g green: CGFloat, b blue: CGFloat, a alpha: CGFloat = 1.0) -> UIColor {
+        return UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: alpha)
+    }
+    
+    ///十六进制颜色
+    public convenience init(hexString: UInt32, alpha: CGFloat = 1.0) {
+        let red     = CGFloat((hexString & 0xFF0000) >> 16) / 255.0
+        let green   = CGFloat((hexString & 0x00FF00) >> 8 ) / 255.0
+        let blue    = CGFloat((hexString & 0x0000FF)      ) / 255.0
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
+
+public extension UIDevice {
+    
+    ///获取设备型号
+    var modelName: String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        
+        switch identifier {
+        case "iPod5,1":                                 return "iPod Touch 5"
+        case "iPod7,1":                                 return "iPod Touch 6"
+        case "iPhone3,1", "iPhone3,2", "iPhone3,3":     return "iPhone 4"
+        case "iPhone4,1":                               return "iPhone 4s"
+        case "iPhone5,1", "iPhone5,2":                  return "iPhone 5"
+        case "iPhone5,3", "iPhone5,4":                  return "iPhone 5c"
+        case "iPhone6,1", "iPhone6,2":                  return "iPhone 5s"
+        case "iPhone7,2":                               return "iPhone 6"
+        case "iPhone7,1":                               return "iPhone 6 Plus"
+        case "iPhone8,1":                               return "iPhone 6s"
+        case "iPhone8,2":                               return "iPhone 6s Plus"
+        case "iPhone8,4":                               return "iPhone SE"
+        case "iPhone9,1":                               return "iPhone 7"
+        case "iPhone9,2":                               return "iPhone 7 Plus"
+        case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":return "iPad 2"
+        case "iPad3,1", "iPad3,2", "iPad3,3":           return "iPad 3"
+        case "iPad3,4", "iPad3,5", "iPad3,6":           return "iPad 4"
+        case "iPad4,1", "iPad4,2", "iPad4,3":           return "iPad Air"
+        case "iPad5,3", "iPad5,4":                      return "iPad Air 2"
+        case "iPad2,5", "iPad2,6", "iPad2,7":           return "iPad Mini"
+        case "iPad4,4", "iPad4,5", "iPad4,6":           return "iPad Mini 2"
+        case "iPad4,7", "iPad4,8", "iPad4,9":           return "iPad Mini 3"
+        case "iPad5,1", "iPad5,2":                      return "iPad Mini 4"
+        case "iPad6,7", "iPad6,8":                      return "iPad Pro"
+        case "AppleTV5,3":                              return "Apple TV"
+        case "i386", "x86_64":                          return "Simulator"
+        default:                                        return identifier
+        }
+    }
+}
+
