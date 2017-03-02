915a7a1082ce1c911ebacdb878788e758b7f7f68
icZzz <icsleepzzz@gmail.com>
2017-03-02 11:20:40 +0800

    ADD CGRect

---
+++ MacroSwift/MacroSwift.h
@@ -0,0 +1,19 @@
+//
+//  MacroSwift.h
+//  MacroSwift
+//
+//  Created by 趙傳龍 on 2017/2/28.
+//  Copyright © 2017年 Huan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MacroSwift.
+FOUNDATION_EXPORT double MacroSwiftVersionNumber;
+
+//! Project version string for MacroSwift.
+FOUNDATION_EXPORT const unsigned char MacroSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MacroSwift/PublicHeader.h>
+
+
